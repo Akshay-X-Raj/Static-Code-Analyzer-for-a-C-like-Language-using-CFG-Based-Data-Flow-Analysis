731d2bab7fd07a970a7997471e7c8d0a61b5cf48
Gyan1110 <gyan94172@gmail.com>
2026-04-29 22:12:21 +0530

    Updated code (bug fixes / improvements)

--- test.c
+++ test.c
@@ -1,13 +1,25 @@
-    int x;
-    int y;
-    int z;
+int x;
+int y;
+int z;
+int p;
+x = 0;
+y = 10;
 
-    x = 0;
-    y = 0;
+while (x < 5) {
 
-    while (x < 5) {
+    if (x < 3) {
         y = y + x;
-        x = x + 1;
+    } else {
+        y = y - x;
     }
 
-    printf("%d", y);
+    if (y > 5) {
+        z = y + x;
+    } else {
+        z = y - x;
+    }
+
+    x = x + 1;
+}
+
+printf("%d", y);
--- utils/Token.h
+++ utils/Token.h
@@ -7,44 +7,44 @@ using namespace std;
 enum TokenType {
 
     // Keywords
-    KW_INT,
-    KW_CHAR,
-    KW_FLOAT,
-    KW_DOUBLE,
-    KW_IF,
-    KW_ELSE,
-    KW_WHILE,
-    KW_PRINTF,
+    KW_INT,        // 0
+    KW_CHAR,       // 1
+    KW_FLOAT,      // 2
+    KW_DOUBLE,     // 3
+    KW_IF,         // 4
+    KW_ELSE,       // 5
+    KW_WHILE,      // 6
+    KW_PRINTF,     // 7
 
     // Identifiers & values
-    IDENTIFIER,
-    NUMBER,
-    STRING,
+    IDENTIFIER,    // 8
+    NUMBER,        // 9
+    STRING,        // 10
 
     // Operators
-    ASSIGN,     // =
-    REL_OP,     // > < >= <= == !=
+    ASSIGN,        // 11   (=)
+    REL_OP,        // 12   (> < >= <= == !=)
 
-    PLUS,       // +
-    MINUS,      // -
-    MUL,        // *
-    DIV,        // /
+    PLUS,          // 13   (+)
+    MINUS,         // 14   (-)
+    MUL,           // 15   (*)
+    DIV,           // 16   (/)
 
     // Symbols
-    SEMICOLON,  // ;
-    LPAREN,     // (
-    RPAREN,     // )
-    LBRACE,     // {
-    RBRACE,     // }
-    COMMA,      // ,
+    SEMICOLON,     // 17   (;)
+    LPAREN,        // 18   ( )
+    RPAREN,        // 19   ( )
+    LBRACE,        // 20   {
+    RBRACE,        // 21    }
+    COMMA,         // 22   (,)
 
     // End
-    END_OF_FILE
+    END_OF_FILE    // 23
 };
 
 // Token structure
 struct Token {
-    TokenType type;
-    string value;
-    int line;   
+    TokenType type;  // token number (from enum)
+    string value;    // actual text (e.g., "int", "x", "5")
+    int line;        // line number in input
 };
